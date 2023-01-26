d3d1b21b932b13f483d727a111cd6b45d665664e
JBxTrade <57236036+JBxTrade@users.noreply.github.com>
2023-01-26 11:41:13 -0500

    Authority

--- src/main/java/com/cftc/sae/security/AuthoritiesConstants.java
+++ src/main/java/com/cftc/sae/security/AuthoritiesConstants.java
@@ -8,7 +8,14 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
-
+    public static final String DIRECTION = "ROLE_DIRECTION";
+    public static final String CP = "ROLE_CP";
+    public static final String SOUTIENT = "ROLE_SOUTIENT";
+    public static final String SAAQ = "ROLE_SAAQ";
+    public static final String REPARTITION = "ROLE_REPARTITION";
+    public static final String HOTEL = "ROLE_HOTEL";
+    public static final String ENSEIGNANT = "ROLE_ENSEIGNANT";
+    public static final String ENTREPRISE = "ROLE_ENTREPRISE";
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
